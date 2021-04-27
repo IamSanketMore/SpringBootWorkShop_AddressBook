f2541633853d774b0611f40ed195f43a0bb0878f
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-27 17:44:19 +0530

    [STARTED]:- Create a Spring Boot Application for create a AddresssBook With API

---
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/AddressbookprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootworkshop.addressbookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressbookprojectApplication.class, args);
+    }
+
+}
