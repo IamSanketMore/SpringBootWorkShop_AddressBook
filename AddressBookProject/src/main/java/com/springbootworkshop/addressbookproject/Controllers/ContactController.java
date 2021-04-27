a4974d8c2635cf7392451042ab8d5540057b7009
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-28 01:57:04 +0530

    [MODIFY] : AutoWired Service Interface and attached to all REST API

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Controllers/ContactController.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Controllers/ContactController.java
@@ -4,56 +4,59 @@ import com.springbootworkshop.addressbookproject.DTO.AddressDTO;
 import com.springbootworkshop.addressbookproject.DTO.ContactDTO;
 import com.springbootworkshop.addressbookproject.DTO.ResponseDTO;
 import com.springbootworkshop.addressbookproject.Model.ContactData;
+import com.springbootworkshop.addressbookproject.Services.IContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contact")
 public class ContactController
 {
+    @Autowired
+    private IContactService iContactService;
+
     @GetMapping(value = { "", "/", "/get" })
     public ResponseEntity<ResponseDTO> getContactData() {
-        ContactData contactData = null;
-        contactData = new ContactData(1,new ContactDTO("Sanket","More","Male",787887787,"sanket@123"));
+        List<ContactData> contactData = iContactService.getContactData();
         ResponseDTO respDTO = new ResponseDTO("Get Call Successful", contactData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @GetMapping(value =  "/getAllContactData" )
     public ResponseEntity<ResponseDTO> getAllContactData() {
-        ContactData contactData = null;
-        contactData = new ContactData(1,new ContactDTO("Sanket","More","Male",787887787,"sanket@123"));
+        List<ContactData> contactData = iContactService.getContactData();
         ResponseDTO respDTO = new ResponseDTO("Get Call Successful", contactData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @GetMapping("/get/{bookId}")
     public ResponseEntity<ResponseDTO> getContactById(@PathVariable("bookId") int bookId) {
-        ContactData contactData = null;
-        contactData = new ContactData(1,new ContactDTO("Sanket","More","Male",787887787,"sanket@123"));
+        List<ContactData> contactData = iContactService.getContactData();
         ResponseDTO respDTO = new ResponseDTO("Get Call Successful", contactData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @PostMapping("/createContact")
     public ResponseEntity<ResponseDTO> addContactData(@RequestBody ContactDTO contactDTO ) {
-        ContactData contactData = null;
-        contactData = new ContactData(1,contactDTO);
+        ContactData contactData = iContactService.addContactData(contactDTO);
         ResponseDTO respDTO = new ResponseDTO("Created Contact Data for", contactData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
-    @PutMapping("/updateContact")
-    public ResponseEntity<ResponseDTO> updateContactData(@RequestBody ContactDTO contactDTO) {
-        ContactData contactData = null;
-        contactData = new ContactData(1,contactDTO);
+    @PutMapping("/updateContact/{bookId}")
+    public ResponseEntity<ResponseDTO> updateContactData(@PathVariable("bookId") int bookId,@RequestBody ContactDTO contactDTO) {
+        ContactData contactData = iContactService.updateContactData(1,contactDTO);
         ResponseDTO respDTO = new ResponseDTO("Created Contact Data for", contactData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteContact/{bookId}")
     public ResponseEntity<ResponseDTO> deleteContactData(@PathVariable("bookId") int bookId) {
+        ContactData contactData = iContactService.deleteContactData(bookId);
         ResponseDTO respDTO = new ResponseDTO("Delete Call Success for id: ", bookId);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
