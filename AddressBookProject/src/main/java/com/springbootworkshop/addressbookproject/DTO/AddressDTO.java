f9c30aeeadf6a329e3e6945207d2c4c33f1d3bdf
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-28 20:12:56 +0530

    [UPDATED] : For AddressDTO Add Regex Patterns and Validation and create Exception for the Address Data

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/DTO/AddressDTO.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/DTO/AddressDTO.java
@@ -2,14 +2,29 @@ package com.springbootworkshop.addressbookproject.DTO;
 
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public @ToString class AddressDTO
 {
+    @NotBlank
     public String address;
-    public String city;
-    public String state;
-    public int zip;
 
-    public AddressDTO(String address, String city, String state, int zip) {
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]{2,}$",message = "!!! Invalid City Name !!! " )
+    public String city;
+
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]{2,}$",message = "!!! Invalid State Name !!! ")
+    public String state;
+
+    @NotNull
+    @Pattern(regexp = "^[1-9]{1}[0-9]{5}$",message = "!!!Invalid Zip Code!!!")
+    public String zip;
+
+    public AddressDTO(String address, String city, String state, String zip) {
         this.address = address;
         this.city = city;
         this.state = state;
