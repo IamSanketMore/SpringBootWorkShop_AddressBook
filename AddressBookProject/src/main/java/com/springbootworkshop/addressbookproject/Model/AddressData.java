33829f36ef74b0111a2fbace6999e142690052a9
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-28 17:25:15 +0530

    [ADDED]: Added @Data Loombook libraray in Address Classs for auto generated for getters and setters

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/AddressData.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/AddressData.java
@@ -1,8 +1,9 @@
 package com.springbootworkshop.addressbookproject.Model;
 
 import com.springbootworkshop.addressbookproject.DTO.AddressDTO;
+import lombok.Data;
 
-public class AddressData
+public @Data class AddressData
 {
     private int addressId;
     private String address;
@@ -18,45 +19,4 @@ public class AddressData
         this.state = addressDTO.state;
         this.zip = addressDTO.zip;
     }
-
-    public int getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(int addressId) {
-        this.addressId = addressId;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
 }
