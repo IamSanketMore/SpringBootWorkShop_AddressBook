05265bd8236dba305307bd7c18ba45409b7937e6
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-28 20:18:00 +0530

    [ADDED] : Create ExceptionHandlers For Handles Exception We throws From Contact And Address DTO

---
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/exceptions/ExceptionHandlers.java
@@ -0,0 +1,35 @@
+package com.springbootworkshop.addressbookproject.exceptions;
+
+import com.springbootworkshop.addressbookproject.DTO.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionHandlers
+{
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception){
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMsg = errorList.stream().map(objectError -> objectError.getDefaultMessage()).collect(Collectors.toList());
+
+        ResponseDTO responseDTO = new ResponseDTO("Exception While processing  REST  REquest",errMsg);
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    protected ResponseEntity<ResponseDTO> handleHttpMessageNotReadable(HttpMessageNotReadableException ex)
+    {
+        String error = "Data Should not be NULL ";
+        ResponseDTO response =new ResponseDTO("Invalid JSON request",error);
+
+    return new ResponseEntity<ResponseDTO>(response, HttpStatus.BAD_REQUEST);
+    }
+}
