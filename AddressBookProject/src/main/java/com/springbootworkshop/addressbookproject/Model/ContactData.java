bc7692297b99f0a8bef702dd233c372fb06bc831
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-28 17:25:37 +0530

    [ADDED]: Added @Data Loombook libraray in Contact Classs for auto generated for getters and setters

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/ContactData.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/ContactData.java
@@ -1,8 +1,9 @@
 package com.springbootworkshop.addressbookproject.Model;
 
 import com.springbootworkshop.addressbookproject.DTO.ContactDTO;
+import lombok.Data;
 
-public class ContactData {
+public @Data class ContactData {
     private int contactId;
     private String firstName;
     private String lastName;
@@ -19,53 +20,5 @@ public class ContactData {
         this.gender = contactDTO.gender;
         this.mobileNumber = contactDTO.mobileNumber;
         this.emailID = contactDTO.emailID;
-    }
-
-    public int getContactId() {
-        return contactId;
-    }
-
-    public void setContactId(int contactId) {
-        this.contactId = contactId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(int mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getEmailID() {
-        return emailID;
-    }
-
-    public void setEmailID(String emailID) {
-        this.emailID = emailID;
     }
 }
