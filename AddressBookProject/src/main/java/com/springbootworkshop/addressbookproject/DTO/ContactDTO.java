1f26992376926ba1e0d01ab5300f5e0d1819f3a2
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-28 16:21:47 +0530

    [MODFIY] : Chnage delete method return type to void

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/DTO/ContactDTO.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/DTO/ContactDTO.java
@@ -1,11 +1,14 @@
 package com.springbootworkshop.addressbookproject.DTO;
 
+import javax.validation.constraints.Email;
+
 public class ContactDTO
 {
     public String firstName;
     public String lastName;
     public String gender;
     public int mobileNumber;
+
     public String emailID;
 
     public ContactDTO(String firstName, String lastName, String gender, int mobileNumber, String emailID) {
