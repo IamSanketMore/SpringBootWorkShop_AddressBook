7839666724fef611bd4410be6ea3bcc2e616200f
IamSanketMore <sanketmore.999.m@gmail.com>
2021-05-01 16:54:35 +0530

    [ADDED] : Entity class and the javax persistance of mysql for the address table in address class

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/AddressData.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/AddressData.java
@@ -2,21 +2,41 @@ package com.springbootworkshop.addressbookproject.Model;
 
 import com.springbootworkshop.addressbookproject.DTO.AddressDTO;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "address_data")
 public @Data class AddressData
 {
-    private int addressId;
+    @Id
+    //private int addressId;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID addressId;
     private String address;
     private String city;
     private String state;
     private String zip;
+    private LocalDateTime createdTimeStamp;
+    private LocalDateTime updatedTimeStamp;
 
-    public AddressData(int addressId, AddressDTO addressDTO) {
+    public AddressData( AddressDTO addressDTO) {
         super();
-        this.addressId = addressId;
+
         this.address = addressDTO.address;
         this.city = addressDTO.city;
         this.state = addressDTO.state;
         this.zip = addressDTO.zip;
     }
+
+    public AddressData() {
+
+    }
+
 }
