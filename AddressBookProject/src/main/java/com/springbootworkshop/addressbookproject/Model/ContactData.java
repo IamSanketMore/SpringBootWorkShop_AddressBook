38dafdf704c62ef8e7bd7417e5afbe82bb977b09
IamSanketMore <sanketmore.999.m@gmail.com>
2021-05-01 16:54:53 +0530

    [ADDED] : Entity class and the javax persistance of mysql for the Contact table in Contact class

--- AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/ContactData.java
+++ AddressBookProject/src/main/java/com/springbootworkshop/addressbookproject/Model/ContactData.java
@@ -2,11 +2,26 @@ package com.springbootworkshop.addressbookproject.Model;
 
 import com.springbootworkshop.addressbookproject.DTO.ContactDTO;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public @Data class ContactData {
-    private int contactId;
+@Entity
+@Table(name = "contact_data")
+public @Data class ContactData
+{
+    @Id
+    //private int contactId;
+    //@Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID contactId;
+
     private String firstName;
     private String lastName;
     private String gender;
@@ -16,13 +31,16 @@ public @Data class ContactData {
     private LocalDateTime updatedTimeStamp;
 
 
-    public ContactData(int contactId, ContactDTO contactDTO) {
+    public ContactData(ContactDTO contactDTO) {
         super();
-        this.contactId = contactId;
         this.firstName = contactDTO.firstName;
         this.lastName = contactDTO.lastName;
         this.gender = contactDTO.gender;
         this.mobileNumber = contactDTO.mobileNumber;
         this.emailID = contactDTO.emailID;
+    }
+
+    public ContactData() {
+
     }
 }
